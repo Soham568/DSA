4647ca4a962ca4e881539496137c82f9a018acaa
Soham568 <sohammahajan568@gmail.com>
2026-08-14 16:03:37 +0530

    Time: 251 ms (48.92%), Space: 42.9 MB (56.21%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,35 @@
+class Solution {
+    boolean solveSudoku(char[][] board){
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                
+                if (board[i][j]=='.') {
+                    for(char c = '1';c <= '9';c++){
+                        if(isValid(board,i,j,c)){
+                            board[i][j] = c;
+                            if(solveSudoku(board)) return true;
+                            board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    boolean isValid(char[][] board,int row,int col,char c){
+        for (int i = 0; i < 9; i++) {
+            if(board[row][i]==c) return false;
+            if(board[i][col]==c) return false;
+        }
+        int boxRowStart = 3 * (row/3);
+        int boxColStart = 3 * (col / 3);
+        for (int i = 0; i < board.length; i++) {
+            if(board[boxRowStart + (i/3)][boxColStart + (i % 3)] == c) return false;
+        }
+        return true;
+    }
+    public void solve(char[][] board) {
+        solveSudoku(board);
+    }
+}
